c9aedec22a537999717ca193118373f4187f71b8
Nitesh Jain <niteshjain9977@gmail.com>
2020-05-11 18:25:38 +0530

    Updates

---
+++ precache-manifest.3bcaf0466b481c5be40d05b2d40650fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f2d96e3ac833a0b4179b109177aefce",
+    "url": "/Contact-Manager/index.html"
+  },
+  {
+    "revision": "e617e52696a663ba15e6",
+    "url": "/Contact-Manager/static/css/2.2ff27ea2.chunk.css"
+  },
+  {
+    "revision": "ca71c22a69b249285f95",
+    "url": "/Contact-Manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "e617e52696a663ba15e6",
+    "url": "/Contact-Manager/static/js/2.e18bf798.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Contact-Manager/static/js/2.e18bf798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca71c22a69b249285f95",
+    "url": "/Contact-Manager/static/js/main.896789a7.chunk.js"
+  },
+  {
+    "revision": "408e55fe3b2f6e5f6095",
+    "url": "/Contact-Manager/static/js/runtime-main.e7cdf879.js"
+  }
+]);
